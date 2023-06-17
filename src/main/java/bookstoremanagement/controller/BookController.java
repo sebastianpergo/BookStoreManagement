c85823f96507fc2d510530b9271ec54015f744ec
sebastianpergo <sebastianperdomopg@gmail.com>
2023-06-16 23:17:38 -0500

    List method done

--- src/main/java/bookstoremanagement/controller/BookController.java
+++ src/main/java/bookstoremanagement/controller/BookController.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.awt.print.Book;
+import java.util.List;
 
 @Controller
 public class BookController {
@@ -25,14 +29,20 @@ public class BookController {
     }
 
     @GetMapping("/listar")
-    public String listarLibros() {
-        return "listarLibros";
+    public ModelAndView listBooks() {
+       List<BookModel> list = service.getBooks();
+       return new ModelAndView("listarLibros", "BookModel", list);
     }
 
     @PostMapping("/save")
     public String addBook(BookModel bookModel) {
         service.save(bookModel);
         return "redirect:/listar";
+    }
+
+    @GetMapping("/listBooks")
+    public List<BookModel> findAll() {
+        return service.getBooks();
     }
 
 }
--- src/main/java/bookstoremanagement/service/BookService.java
+++ src/main/java/bookstoremanagement/service/BookService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 @Service
 public class BookService {
     @Autowired
@@ -14,6 +16,10 @@ public class BookService {
     // Save method
     public void save(BookModel book) {
         repo.save(book);
+    }
+
+    public List<BookModel> getBooks() {
+        return repo.findAll();
     }
 
 }
